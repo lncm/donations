dbc56d28e86a10808b0f9bb41712e47c30d403f7
Damian Mee <spam@meedamian.com>
2019-04-25 13:53:45 +0700

    fix a units bug (sats/milisats) where invoices were requested for amounts bigger than 2^32/1e3 causing errors to happen

--- src/config.js
+++ src/config.js
@@ -8,6 +8,6 @@ export const INFO_URL = `/api/info`;
 // Used in Invoice descriptions, as well as header of the website
 export const RECIPIENT = 'LNCM';
 
-// Defines max allowed LN payment.  Max 2^32 per payment is currently defined in
-// the spec
-export const MAX_LN_PAYMENT = 2 ** 32; // = 4294967;
+// Defines max allowed LN payment.  Max 2^32 millisatoshis per payment is
+// currently defined in the spec
+export const MAX_LN_PAYMENT = 2 ** 32 / 1e3; // = 4294967;
